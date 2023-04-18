4fab90df9cf21eeb0f67ed878de07b603b81cd2f
Larysa Bilan <114293094+dudetu@users.noreply.github.com>
2023-04-19 00:12:14 +0200

    Add files via upload

---
+++ jeneric/src/Main.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+
+import static java.lang.String.valueOf;
+import static java.time.LocalDate.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+
+        Jeneric.DataBaseWriter<String> dataBaseWriter = new Jeneric.DataBaseWriter<>("MySQL", "Hello World!");
+                  // выводит: Данные в базу данных  MySQL записаны, тип данных = String
+
+        Jeneric.DataBaseWriter<Integer> dataBaseWriter1 = new Jeneric.DataBaseWriter<>("MySQL", 123);
+        // выводит: Данные в базу данных MySQL записаны, тип данных = Integer
+
+        Jeneric.DataBaseWriter<LocalDate> dateDateDataBaseWriter = new Jeneric.DataBaseWriter<>("PostgreSQL", LocalDate.now());
+
+        dataBaseWriter.save();
+        dataBaseWriter1.save();
+        dateDateDataBaseWriter.save();
+
+    }
+}
